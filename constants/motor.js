bb3961a28b3fb385435190d11a50dad38fd777bf
Abdelrahman Ashraf <mr.abdo1213@gmail.com>
2024-06-18 18:23:08 +0400

    hasa

--- constants/motor.js
+++
@@ -1,5 +0,0 @@
-const motor = {
-    kgPerSec: .4, // 0.4 kg/sec
-}
-
-module.exports = motor
--- constants/sunsetsunrise.js
+++
@@ -1,8 +0,0 @@
-const models = require("../models")
-
-const SunsetSunriseData = [];
-const RunSunsetSunriseDataImport = () => {
-    const sql_trunc = "";
-    const sql_insert = "";
-    // models.SunriseSunsets.create();
-}
